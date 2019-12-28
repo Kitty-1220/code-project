8cf86d05e9b9783e5def311e821ad44829ff758c
lion <RainbowtoFuture2020@outlook.com>
2019-12-28 10:52:21 +0000

    conclusion

--- blogapi/blog_project/settings.py
+++ blogapi/blog_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -141,3 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+SWAGGER_SETTING = {
+    'LOGIN_URL': 'rest_framework:login',
+    'LOGOUT_URL': 'rest_framework:logout',
+}
--- blogapi/blog_project/urls.py
+++ blogapi/blog_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_schema_view(title='Blog Api')
+API_TITLE = 'Blog API'
+API_DESCRIPTION = 'A Web API for creating and editing blog posts.'
+schema_view = get_swagger_view(title=API_TITLE)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +29,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('schema/', schema_view),
+    #path('schema/', schema_view),
+    path('swagger-docs/', schema_view),
+    path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)), 
 ]
